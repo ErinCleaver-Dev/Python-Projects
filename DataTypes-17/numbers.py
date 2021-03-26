1267cf0ca2e8c41e6748ed4239e8b6bcd2de231c
ErinCleaver-GRCC <77419665+ErinCleaver-GRCC@users.noreply.github.com>
2021-03-26 09:26:54 -0400

    update

--- DataTypes-17/numbers.py
+++ DataTypes-17/numbers.py
@@ -3,6 +3,12 @@
 print(type(2 + 4));
 print(2-4);
 print(2*4);
-print(2/4);
 
 #float stands for a floating point number
+#Takes this more memory.
+print(2/4);
+#used to get power.
+print(2 ** 2);
+#used to get whole numbers from division 
+print(5//4);
+
